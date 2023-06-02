a41420e8f5a8be1ce791a13a18e7ee580407d015
mohammad.vahedi <mohammad.vahedi@zoodfood.com>
2023-06-03 00:35:44 +0330

    Add word service

---
+++ internal/services/service/word.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/mvahedii/wordbox/internal/repositories"
+	"github.com/mvahedii/wordbox/internal/repositories/models"
+	"github.com/mvahedii/wordbox/internal/services/dto"
+)
+
+type PackageService struct {
+	wr repositories.WordRepository
+}
+
+func toDto(w *models.Word) *dto.Word {
+	return &dto.Word{
+		ID:        w.ID,
+		Title:     w.Title,
+		Def:       w.Def,
+		CreatedAt: w.CreatedAt,
+	}
+}
+
+func fromDto(w *dto.Word) *models.Word {
+	return &models.Word{
+		ID:        w.ID,
+		Title:     w.Title,
+		Def:       w.Def,
+		CreatedAt: w.CreatedAt,
+	}
+}
+
+func NewPackageService(r repositories.WordRepository) *PackageService {
+	return &PackageService{
+		wr: r,
+	}
+}
+
+func (repo *PackageService) Create(ctx context.Context, wordDTO *dto.Word) error {
+	w := fromDto(wordDTO)
+	err := repo.wr.Create(ctx, w)
+	if err != nil {
+		return errors.New("error on repo create ")
+	}
+	return nil
+
+}
